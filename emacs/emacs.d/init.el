2b03ec49b5eeff458a9e66554e6365114d264d5c
Ari Lerner <arilerner@mac.com>
2012-06-04 12:27:23 -0700

    Added emacs

---
+++ emacs/emacs.d/init.el
@@ -0,0 +1,18 @@
+(add-to-list 'load-path "~/.emacs.d/packages/")
+
+; marmalade
+(require 'package) 
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/")) (package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-eshell
+                      starter-kit-bindings scpaste
+                      clojure-mode clojure-test-mode
+                      markdown-mode yaml-mode tuareg                                                                                                                                  
+                      marmalade oddmuse scpaste))
+
+(dolist (p my-packages)
+    (when (not (package-installed-p p))
+          (package-install p)))
