8d9c2bfa5353893910fa7a0aee33f1e2c388c0a9
Ari Lerner <arilerner@mac.com>
2012-06-05 09:29:55 -0700

    Updated emacs

--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -10,9 +10,37 @@
 (defvar my-packages '(starter-kit starter-kit-lisp starter-kit-eshell
                       starter-kit-bindings scpaste
                       clojure-mode clojure-test-mode
+                      paredit
                       markdown-mode yaml-mode tuareg                                                                                                                                  
                       marmalade oddmuse scpaste))
 
 (dolist (p my-packages)
     (when (not (package-installed-p p))
           (package-install p)))
+
+(setq-default inhibit-startup-screen t)
+
+;; Of course, don't uncomment the line below -- doing so would
+;; stop Emacs from helpfully leaving "foo~" (backup) files all
+;; over the place.
+;(setq make-backup-files nil)
+
+;; Use only spaces (no tabs at all).
+(setq-default indent-tabs-mode nil)
+
+;; Always show column numbers.
+(setq-default column-number-mode t)
+
+;; Display full pathname for files.
+(add-hook 'find-file-hooks
+          '(lambda ()
+             (setq mode-line-buffer-identification 'buffer-file-truename)))
+
+;; For easy window scrolling up and down.
+(global-set-key "\M-n" 'scroll-up-line)
+(global-set-key "\M-p" 'scroll-down-line)
+
+;; Comment this out if you'd prefer the default color theme, or
+;; change it if you'd prefer a different color theme.
+(load-theme 'zenburn t)
+
