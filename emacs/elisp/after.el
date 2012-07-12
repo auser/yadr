bc040778d76a23696ec2034ec167c5f7c01af1ed
Ari Lerner <arilerner@mac.com>
2012-07-12 11:04:09 -0700

    Updates -- as always

--- emacs/elisp/after.el
+++ emacs/elisp/after.el
@@ -37,8 +37,15 @@
 (define-key global-map (kbd "RET") 'newline-and-indent)
 
 ;; Set the theme
+(require 'color-theme)
 (load-theme 'solarized-dark t)
 
+;; CommandT
+(defun find-dired-project (dir)
+  (interactive "D")
+  (find-dired dir "-no -path '*/.svn*' -not -path '*/.git*' -and -not -path '*.o' -and -type f"))
+
+(global-set-key "\C-xd" 'find-dired-project)
 
 ;; Compilation
 (defun my-save-and-compile ()
--- emacs/emacs.d/init.el
+++ emacs/emacs.d/init.el
@@ -23,6 +23,10 @@
     (when (not (package-installed-p p))
           (package-install p)))
 
+;; trying el-get
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")  (unless (require 'el-get nil t) (url-retrieve "https://raw.github.com/dimitri/el-get/master/el-get-install.el" (lambda (s) (goto-char (point-max)) (eval-print-last-sexp))))
+
+
 (setq-default inhibit-startup-screen t)
 (autoload 'slime-selector "slime" t)
 
