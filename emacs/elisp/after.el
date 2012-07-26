d8c7645cf5f19b1cbcbcb64379e2397e2dc262de
Ari Lerner <arilerner@mac.com>
2012-07-26 14:29:54 -0700

    Updated theme

--- emacs/elisp/after.el
+++ emacs/elisp/after.el
@@ -40,6 +40,17 @@
 ;;(require 'color-theme)
 ;;(load-theme 'solarized-dark t)
 
+;; el-get
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+(unless (require 'el-get nil t)
+  (url-retrieve
+    "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
+    (lambda (s)
+      (end-of-buffer)
+      (eval-print-last-sexp))))
+
+(el-get 'sync)
+
 ;; CommandT
 (defun find-dired-project (dir)
   (interactive "D")
