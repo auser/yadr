1ad68632e1f196a4de764a9f896805a375abab50
Ari <ari@fullstack.io>
2018-06-28 17:05:25 -0400

    Updated gopath

--- zsh/golang.zsh
+++ zsh/golang.zsh
@@ -1,3 +1,9 @@
 export GOENV_ROOT="$HOME/.goenv"
 export PATH="$GOENV_ROOT/bin:$PATH"
+
+# export GOROOT=$HOME/go
+export GOBIN=$GOROOT/bin
+export PATH=$PATH:$GOROOT/bin
+export GOPATH=$HOME/go
+
 eval "$(goenv init -)"
