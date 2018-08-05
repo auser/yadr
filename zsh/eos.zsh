46b1ac7cc7c47928711cc3dfdf6c749fe3eaba77
Ari <ari@fullstack.io>
2018-08-05 14:20:13 -0400

    added eos build tools

--- zsh/eos.zsh
+++ zsh/eos.zsh
@@ -3,7 +3,7 @@ alias cleos='docker exec eosio /opt/eosio/bin/cleos --wallet-url http://localhos
 alias start_eos='docker run --rm --name eosio -p 8888:8888 -p 9876:9876 -v /tmp/work:/work -v /tmp/eosio/data:/mnt/dev/data -v /tmp/eosio/config:/mnt/dev/config eosio/eos-dev  /bin/bash -c "nodeos -e -p eosio --plugin eosio::wallet_api_plugin --plugin eosio::wallet_plugin --plugin eosio::producer_plugin --plugin eosio::history_plugin --plugin eosio::chain_api_plugin --plugin eosio::history_api_plugin --plugin eosio::http_plugin -d /mnt/dev/data --config-dir /mnt/dev/config --http-server-address=0.0.0.0:8888 --access-control-allow-origin=* --contracts-console"'
 alias stop_eos='docker ps | grep eos awk "{print $1}" | xargs docker kill'
 
-
+export PATH="/Users/auser/Development/eos/open-source/eos/build/tools:$PATH"
 
 
 # export DC_EOS="/Users/auser/Development/eos/open-source/eos/Docker/docker-compose.yml"
