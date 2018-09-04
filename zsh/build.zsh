6e514f4823f9850b3dba7ef5523f15a070fce3ec
Ari <ari@fullstack.io>
2018-09-04 15:45:08 -0400

    Updates

--- zsh/build.zsh
+++ zsh/build.zsh
@@ -1,3 +1,5 @@
 export LLVM_DIR=/usr/local/Cellar/llvm/6.0.1/lib/cmake
 export LDFLAGS="-L/usr/local/opt/llvm@4/lib -Wl,-rpath,/usr/local/opt/llvm@4/lib"
 export PATH="/usr/local/opt/llvm@4/bin:$PATH"
+
+alias antlr='java -jar ~/.yadr/bin/antlr-4.7-complete.jar'
