b7a633286a0499e19bc7e22bdf04b1dd2bf9cc0f
Ari Lerner <arilerner@mac.com>
2019-05-28 11:25:38 -0400

    Updated midway auth to be a function, instead of automatic

--- zsh/amazon.zsh
+++ zsh/amazon.zsh
@@ -1,6 +1,8 @@
 export PATH=/usr/local/amazon/bin:$PATH
 export PATH=$HOME/.toolbox/bin:$PATH
 
+function midway_auth() {
+
   SSH_CERT=~/.ssh/id_rsa-cert.pub
   if (! test -f "$SSH_CERT") || (test "`find ~/.ssh/id_rsa-cert.pub -mmin +1220`"); then
     echo "Midway expired"
@@ -11,4 +13,7 @@ export PATH=$HOME/.toolbox/bin:$PATH
       echo "Failed to authenticate."
       exit 1
     fi
+  else
+    echo "Midway currently authenticated"
   fi
+}
